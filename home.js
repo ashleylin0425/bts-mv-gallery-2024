873c7954e7db0faeedd0f62e7a4dfe11d9271bdd
ashleylin0425 <157069906+ashleylin0425@users.noreply.github.com>
2024-04-02 13:40:40 -0400

    Home page update 1

---
+++ home.js
@@ -0,0 +1,6 @@
+function toggleSidebar() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = (sidebar.style.display === 'none' || sidebar.style.display === '') ? 'flex' : 'none';
+  }
+  
+  
