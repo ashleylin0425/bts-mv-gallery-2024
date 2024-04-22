fe8efd4329c986d6abc414856549bcec0738a183
ashleylin0425 <157069906+ashleylin0425@users.noreply.github.com>
2024-04-22 16:36:15 -0400

    Update Album html

--- songsData.js
+++ songsData.js
@@ -4,30 +4,100 @@ const songs = ["No More Dream", "N.O", "Just one day"];
 const songData = {
     "No More Dream": {
         "album": "2 COOL 4 SKOOL",
-        "youtubeApiCode": "l15_-4-_QO8"
+        "youtubeApiCode": "l15_-4-_QO8",
+        "description": `The music video for BTS's "No More Dream" may be interpreted as a representation of the transition from childhood to adolescence, when children are encouraged to dream big but teenagers are no longer. The song's lyrics encourage listeners to rebel against the idea that they need to know what they're doing and where they're headed, and to choose their own dreams over boring expectations. The song also mocks society's tendency to tell younger generations to "dream big" and "follow your dreams" but then stop them from doing so.`
     },
 
-    "N.O": {
-        "album": "O!RUL8,2?",
-        "youtubeApiCode": "r5GaAEHvHj0"
-    },
-
-    "Just one day":
+    "Boy In Luv":
     {
         "album": "SKOOL LUV AFFAIR",
-        "youtubeApiCode": "dzAwJW107nk"
+        "youtubeApiCode": "LHHjp3IMjMk",
+        "description": `The music video (MV) for BTS's song "Boy in Luv" is about a group of boys chasing a girl at school. Some say the song is about a young boy who is in love and doesn't know how to act. Others say the song is about how BTS's ideas of love evolve over five years, from wanting to be loved to accepting love from others and themselves.`
     },
 
-    "Boy With Luv":
 
+    "I NEED U":
+    {
+        "album": "The Most Beautiful Moment In Life Pt.1",
+        "youtubeApiCode": "jjskoRh8GTE",
+        "description": `The music video (MV) for BTS's song "I NEED U" is an allegory for the challenges of growing up and the difficulty of letting go of youth. The song is about the end of a relationship between two lovers, and in the MV, it's about the end of a young person's relationship with their youth, happiness, and life. The MV's timeline is important because it clarifies what's happening and adds meaning to symbolic scenes. A cigarette may represent someone thinking that smoking feels as sweet as candy, or that they have schizophrenia and think the cigarette is a lollipop; A scene with fire everywhere may symbolize an explosion at a gas station; Suga setting a room on fire may symbolize giving up on life and turning to self-immolation as an escape; The act of destruction may be an expression of will and individuality, rather than vandalism.`
+
+    },
+
+    "Blood Sweat & Tears":
+    {
+        "album": "Wings",
+        "youtubeApiCode": "hmE9f-TEutc",
+        "description": `The video starts with the members of BTS: RM, Jin, Suga, J-Hope, Jimin, V, Jungkook frolicking in what looks like a museum exhibit or the lobby of a glamorous manor. Even surrounded by gleaming marble pillars and artfully posed statues, the boys don't look the slightest bit out of place in their trappings of velvet brocade topped with sequin embellishments. No doubt, the set and costumes reeking of material excess are a gesture towards the level of desperation tingeing the song, in that if all this luxury still cannot satisfy BTS, they must be craving something that necessitates the sacrifice of possessions much more intimate and precious than money. From their “blood, sweat, and tears” to their “body, mind, and soul,” BTS seems to invite either their lover or a higher power to “take them all away.”`
+
+    },
+
+    "Spring Day":
+    {
+        "album": "You Never Walk Alone",
+        "youtubeApiCode": "xEeFrLSkMm8",
+        "description": `The music video for “Spring Day” makes good use of the juxtaposition of individual scenes and group scenes, representing both the loneliness and hope of the song. We can see the theme of loneliness play out right from the start, as Taehyung stands alone at a train station, putting his ear to the tracks to listen for an oncoming train. The wide shots of him in the snow showcase the emptiness of the station, representative of loneliness. 
+
+        Others are pictured alone as well, with Jungkook alone on the train, Jimin alone by the sea (again with a shot that shows him as only a tiny fraction of his surroundings), and Namjoon also alone on the train. Namjoon's scene transitions to a hotel with a large Omelas sign, perhaps referencing Omelas as a temporary place that will eventually be left. The scene he steps out into shows snow on the ground, a reference to the winter discussed in the lyrics. He walks right past Yoongi and Hoseok, and later on the train he walks past Jungkook's car without stopping, as if it were a missed opportunity to reconnect, because winter has yet to thaw. 
+        
+        Scenes of loneliness are cut with scenes of the members together, such as the scenes where they celebrate Namjoon's birthday. These appear a few times throughout the video, showing the members all together or with at least one other person. More scenes depict the members alone: Seokjin as he watches the others climb the stairs, Jungkook alone in front of a dilapidated ride reading “You Never Walk Alone,” and more wide shots of Taehyung and Jimin. 
+        
+        Jimin picks up some shoes on the beach, which we later also see beside him at the laundromat. These may be symbolic of memories that are left behind, like living with the ghost of someone. Yoongi's solo shot shows him lying listlessly among a huge mountain of laundry, where he looks as if he were cast aside like the clothes. Hoseok's solo shot has him on top of the train, going wherever it takes him. 
+        
+        Jungkook is shown at the dilapidated ride again, but this time, the others move around him as if he's stuck in slow motion. But, he ends up moving from his spot and joining the others. Soon after, he's shown running through the sets of the music video (Omelas, the laundromat), eventually arriving to where all the others are gathered. (Taehyung is missing from this scene, but we learned from the WINGS tour press conference that he just forgot to get in the shot…) Like a cascading effect, the members begin to join others in scenes where they had been depicted alone, such as on the mountain of laundry with Yoongi as well as the scene where Jungkook's match goes out when he's alone, but when it reignites, the others are there. This scene appears with the lyrics “no darkness, no season / can last forever.” 
+        
+        The members are then shown reunited together on the train where Jungkook had previously been alone. The moving train, which appears many times throughout the music video, may be understood as symbolic of the flow of time, which is also mentioned in the lyrics. The BTS members are passengers on the train; by not actively participating in moving the train forward, they are simply along for the ride. This meshes well with the idea of moving through time in a passive manner without actively doing anything to fix one's relationships. I think that's a universal situation many can relate to. By the end of the music video, they all get off the train, which has carried them through winter to an open field that is beginning to thaw, with a singular tree representative of life and the potential for rebirth in spring. 
+        
+        The inclusion of the train can also be a reference to the use of “snowpiercer” in the lyrics (and it's interesting to note that the film Snowpiercer also has a dark secret related to the suffering of children, like the story of “Omelas.”) 
+        Walking across the open field appears to be symbolic of walking away from Omelas, though they walk together instead of alone. Jimin places the shoes into the tree, which can be taken as symbolic of moving on from one's longing and grief. (Note: In some cultures, it has been suggested that shoes tied together over a wire or tree branch can mean many things, such as a death or a mark for gang territory. There is no real definitive consensus as to what this means, and it likely has many meanings to different people.) 
+        
+        At the end, they have found their spring day together.`
+    },
+
+    "FAKE LOVE":
+    {
+        "album": `LOVE YOURSELF 轉 'TEAR'`,
+        "youtubeApiCode": "7C2z4GqqS5E&t=1s",
+        "description": `The music video (MV) for "Fake Love" has multiple interpretations: Her love is fake, My appearance in the mirror is fake, Her existence is fake, Each member's trauma, and Jungkook matures and creates a fake self. In the extended version of the MV, Jungkook enters a "secret passage" and joins the other members' fake selves in an attempt to gain love. However, they eventually crush their fake selves to find real love.`
+    },
+
+    "IDOL":
+    {
+        "album": `LOVE YOURSELF 結 'ANSWER'`,
+        "youtubeApiCode": "pBuZEGYXA6E&t=1s",
+        "description": `IDOL combines traditional and modern concepts, with traditional Korean instruments and opera alongside modern electronic dance music and references to Hollywood movies. The video also includes references to Korean folklore, such as the rabbit in the moon, which is said to be preparing the recipe for immortality. Some interpret the video as BTS showing haters that they can't stop them from loving themselves. For example, during Namjoon's part, he puts filters on his face to show that he doesn't care what people say about him.`
+    },
+
+
+    "Boy With Luv":
     {
         "album": "Map of the Soul: Persona",
         "youtubeApiCode": "XsX3ATc3FbA",
-        "description": "The music video for BTS's Boy With Luv tells a story of a boy and girl, with the girl representing ARMY and the boy representing BTS. The video includes clips from the Speak Yourself: Love Yourself Tour. The boy starts out sad and lonely, but the girl comes into his life and stays with him. The video also includes references to the 1952 musical movie Singing in the Rain. The song Boy With Luv is about finding joy in the smallest things in life, and how true strength and love come from that. RM has explained that the song is about matured love, while their 2014 song Boy In Luv is about teen love."
+        "description": `The music video for BTS's "Boy With Luv" tells a story of a boy and girl, with the girl representing ARMY and the boy representing BTS. The video includes clips from the Speak Yourself: Love Yourself Tour. The boy starts out sad and lonely, but the girl comes into his life and stays with him. The video also includes references to the 1952 musical movie Singing in the Rain. The song "Boy With Luv" is about finding joy in the smallest things in life, and how true strength and love come from that. RM has explained that the song is about matured love, while their 2014 song Boy In Luv is about teen love.`
 
-    }
+    },
+    
+    "Dynamite":
+    {
+        "album": `Dynamite`,
+        "youtubeApiCode": "gdZLi9oWNZg&t=1s",
+        "description": `The lyrics of "Dynamite" are "a much-needed burst of joy and invitation to join them, dance, and be a light, even when the world seems a little dark". RM, a member of BTS, described the song as "made of positive vibes, energy, hope, love, the purity, everything". The song is about the joy and appreciation of small things that make life valuable, and includes references to pop culture, such as King Kong, The Rolling Stones, and LeBron James.`
+    },
 
-}
+    "Life Goes On":
+    {
+        "album": `BE`,
+        "youtubeApiCode": "-5q5mZbe3V8&t=2s",
+        "description": `The music video for "Life Goes On" begins with a close-up of Tae driving, which is a motif that continues throughout the video. Some say that Tae represents the journey of doubt, while others say that the song is a message of hope to ARMY during difficult times. The song was written to reassure ARMY that people shouldn't give up, even though it was difficult to live during COVID. The first half of the song expresses the group's disappointment with the pandemic, while the second half shows them having a more positive outlook.`
+    },
+
+    "Wild Flower":
+    {
+        "album": `Indigo`,
+        "youtubeApiCode": "u18be_kRmC0",
+        "description": `It seems RM hasn't yet been able to reach the point where he can look back and applaud and smile, and he may not ever get there, as he goes on to deliver the lines, "When everything I believed in grew distant / When all this fame turned into shackles / Please take my desire away from me." Despite his unfulfilled desires, RM still strives to embrace his true self, and these existential themes echo throughout "Indigo." In the chorus, sung beautifully by Youjeen, RM encourages himself (and, in doing so, his listeners) to shine by lighting a "flowerwork." The video that accompanies "Wild Flower" is a visual masterpiece. It sees RM deliver his verses in various scenic but caustic locations: the eye of a storm, a dark space where fire embers fall from the sky, and a packed stadium — all juxtaposed against shots of the BTS leader standing in a serene lavender field. Fittingly, fireworks explode throughout the video, a callback to RM's encouraging term "flowerworks."`
+    },
+
    
 
-
+}
