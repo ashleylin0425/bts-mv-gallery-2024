4cdda4c30cfd194afe71c004e99fad638fb3a76e
ashleylin0425 <157069906+ashleylin0425@users.noreply.github.com>
2024-04-26 10:54:49 -0400

    finish the about html

--- home.js
+++
@@ -1,6 +0,0 @@
-function toggleSidebar() {
-    var sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = (sidebar.style.display === 'none' || sidebar.style.display === '') ? 'flex' : 'none';
-  }
-  
-  
--- js/videoplayer.js
+++ js/videoplayer.js
@@ -1,16 +1,13 @@
 function embedYouTubeVideo(apiKey, videoId) {
-    // Create an iframe element for the YouTube video player
-    var iframe = document.createElement("iframe");
+    let iframe = document.createElement("iframe");
     iframe.setAttribute("src", "https://www.youtube.com/embed/" + videoId + "?enablejsapi=1&autoplay=1&mute=1&origin=https://ashleylin0425.github.io/bts-mv-gallery-2024/");
     iframe.setAttribute("width", "100%");
     iframe.setAttribute("height", "450");
     iframe.setAttribute("frameborder", "0");
     iframe.setAttribute("allowfullscreen", "true");
   
-    // Get the video player container element by its ID
-    var videoPlayerContainer = document.querySelector(".video-container");
+    let videoPlayerContainer = document.querySelector(".video-container");
   
-    // Append the iframe to the video player container
     videoPlayerContainer.appendChild(iframe);
   }
   
